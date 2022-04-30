b909563e862e7e9f433e0f2979f38e2d66b7f644
taekionic <98569426+taekionic@users.noreply.github.com>
2022-04-30 16:38:10 -0400

    project 2

---
+++ Basic Javascript Projects/Project2_functions/js/main.js
@@ -0,0 +1,9 @@
+function multiply() {   //defines the function named "multiply"
+    var solution = 5*6; //devines a variable named solution, which is equal to the product of 5 and 6
+    solution += " is the solution!" //concatenates the string "is the solution" to the variable solution.
+    document.getElementById('click'); //calls the  element with the id 'click'. Here, this is <button>
+    document.write(solution); //prints the solution variable on function execution.
+    
+}
+    
+
