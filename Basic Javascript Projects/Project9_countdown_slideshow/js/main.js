3556d829aaba84148dced261020005addd7308f6
taekionic <98569426+taekionic@users.noreply.github.com>
2022-05-05 19:26:12 -0400

    project 9

--- Basic Javascript Projects/Project9_countdown_slideshow/js/main.js
+++ Basic Javascript Projects/Project9_countdown_slideshow/js/main.js
@@ -1,13 +1,13 @@
 function countdown() {
-    var seconds = document.getElementById("seconds").value;
+    var seconds = document.getElementById("seconds").value; //referencing input from our input box
 
     function tick() {
         seconds = seconds - 1;
         timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
-        if (seconds == -1) {
+        var time = setTimeout(tick, 1000); //setting a ticker to the value of 1000 ms, or 1 second. 
+        if (seconds == -1) { //defining an if function that will return the alert "times up" if our ticker attempts to count below 0
             alert("Time's up!");
-            clearTimeout(time);
+            clearTimeout(time); //resets our timer
             timer.innerHTML = "";
         }
     }
@@ -19,7 +19,7 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n); //pushes our image forward or backward one index in our slideshow
 }
 
 // Thumbnail image controls
@@ -29,7 +29,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("mySlides"); //sets our slides and calls the relevant element by its defined class
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
